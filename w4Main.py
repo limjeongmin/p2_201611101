1a31be241042a577e9bb4f0d14805c7aab4d3c34
limjeongmin <jml8740@naver.com>
2016-03-29 22:58:55 +0900

    assignment
    assignment

---
+++ w4Main.py
@@ -0,0 +1,15 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def makeSwirlsSquare(size,bigger,sides,angle):
+    for i in range(0,sides):
+        if(i%2):
+            size=size+bigger
+            t1.fd(size)
+            t1.right(angle)
+        else:
+            size=size
+            t1.fd(size)
+            t1.right(angle)
+makeSwirlsSquare(10,10,30,90)
+wn.exitonclick()
