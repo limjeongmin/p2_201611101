965612ae19a532cf26021ff1cdb39ab71a302e33
limjeongmin <jml8740@naver.com>
2016-04-03 21:33:52 +0900

    Added files via upload

---
+++ w5Main3.py
@@ -0,0 +1,22 @@
+﻿@startuml
+title Conditional -Who is winner the RockScissorsPaper?
+start
+:get userA input a;
+:get userB input b;
+if (rock win);
+  :rock or scissors;
+  :rock select user is winner;
+elseif (paper win);
+  :paper or rock;
+  :paper select user is winner;
+elseif (scissors win);
+  :scissors or paper;
+  :scissors select user is winner;
+elseif (draw);
+  :rock or rock;
+  :scissors or scissors;
+  :paper or paper;
+  :No winner;
+endif
+stop
+@enduml
