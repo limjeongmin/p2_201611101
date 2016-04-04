96ac16d209199ad17a5bd14faba77b408bc17e3b
limjeongmin <jml8740@naver.com>
2016-04-04 17:09:37 +0900

    Added files via upload

---
+++ w5Main_8.py
@@ -0,0 +1,24 @@
+﻿@startuml
+
+title  <BMI calculator> 
+
+
+start
+
+:bmi=weight/ (height*height); 
+if(bmi=<18.5)
+:Under weight;
+elseif(18.5<bmi=<23)
+:nomarl weight;
+elseif(23<bmi=<25)
+:over weight;
+elseif(25<bmi=<30)
+:obesity;
+elseif(30<bmi=<35)
+:very obesity;
+else (35<bmi)
+  :extremly obesity;
+endif
+stop
+
+@enduml
