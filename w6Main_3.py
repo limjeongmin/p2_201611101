458df87180cad9b63f3d1ef1ab58639d018c9a55
limjeongmin <jml8740@naver.com>
2016-04-11 15:53:29 +0900

    Added files via upload

---
+++ w6Main_3.py
@@ -0,0 +1,16 @@
+﻿def sumList(aList):
+    sum=0
+    for k in range(0,len(x)):
+        sum=sum+x[k]
+    print sum
+
+    return sum
+print sum
+    
+def lab6():
+    """programmin is really ha ha ha fun"""
+    aList=[x]
+    labsum=sumList(aList)
+    print labsum
+def main():
+    lab6()
