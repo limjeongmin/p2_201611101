d5c61d9658bccf0854e49bdddb2177e436b21df3
limjeongmin <jml8740@naver.com>
2016-06-02 11:47:51 +0900

    강아지 소리내기

---
+++ w14Main_1.py
@@ -0,0 +1,30 @@
+﻿class Dog(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "mt dog name is ",self.name
+    def talk(self): 
+        print "mung mung" 
+         
+class ShihtzuDog(Dog): 
+    def talk(self): 
+        print "si si" 
+         
+class Maltese(Dog): 
+    def talk(self): 
+        print "mal mal"
+def dogsound():
+    mydog1=Dog('poppy') 
+    mydog2=ShihtzuDog('poppy') 
+    mydog3=Maltese('poppy') 
+    mydog1.talk() 
+    mydog2.talk() 
+    mydog3.talk()
+
+def lab14():
+    dogsound()
+def main():
+    lab14()
+
+if__main__=="__main__":
+    main()
